f9aac0d292060288c34a14f931af88a46959493f
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-06-15 02:35:11 +0530

    feat: implement JWT authentication for post creation

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,6 +1,8 @@
 const postModel = require('../models/Post.model')
 const Imagekit = require('@imagekit/nodejs')
 const { toFile } = require('@imagekit/nodejs')
+const jwt = require('jsonwebtoken')
+
 
 
 const imagekit = new Imagekit({
@@ -10,12 +12,41 @@ const imagekit = new Imagekit({
 async function createPostController(req, res){
     console.log(req.body, req.file)
 
-    const file = await imagekit.files.upload({
-        file: await Imagekit.toFile(Buffer.from(req.file.buffer), "image"),
-        fileName: "image"
+    const token = req.cookies["jwt_token"];
+
+    if(!token) return res.status(401).json({
+        messsage: "Token not provided, Unauthorised Access"
     })
 
-    res.send(file)
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+    } catch (error) {
+        return res.status(401).json({
+            message: "Unauthorised Access"
+        })
+    }
+
+    
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), "image"),
+        fileName: "image",
+        folder:"Social-Posts"
+    })
+    
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+
+    
 }
 
 
