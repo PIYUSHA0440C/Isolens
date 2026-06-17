0a81d04efe79a6d731fcc5257266f36e66ae0644
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-06-18 01:41:43 +0530

    feat: add post retrieval and details functionality with JWT authentication

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -2,12 +2,17 @@ const postModel = require('../models/Post.model')
 const Imagekit = require('@imagekit/nodejs')
 const { toFile } = require('@imagekit/nodejs')
 const jwt = require('jsonwebtoken')
+const { default: mongoose } = require('mongoose')
 
 
 
 const imagekit = new Imagekit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
+
+
+
+// Controllers
 
 async function createPostController(req, res){
     console.log(req.body, req.file)
@@ -50,6 +55,88 @@ async function createPostController(req, res){
 }
 
 
+async function getPostController(req, res) {
+
+    const token = req.cookies["jwt_token"];
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Token not provided, Unauthorised Access"
+        })
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message: "Unauthorised Access"
+        })
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "Posts fetched successfully",
+        posts
+    })
+}
+
+
+async function getPostDetails(req, res) {
+    
+    const token = req.cookies["jwt_token"]
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Token not provided, Unauthorised Access"
+        })
+    }
+
+    let decoded;
+    try{
+
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid Token, Unauthorised Access"
+        })
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId);
+
+    if(!post){
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId;
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    res.status(200).json({
+        message: "Post fetched successfully.",
+        post
+    })
+}
+
+
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetails
 }
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -10,6 +10,18 @@ const upload = multer({storage: multer.memoryStorage()});
 // POST /api/posts [protected]
 // -req.body = {caption, image-file}
 
-postRouter.post("/", upload.single("image"), postController.createPostController)
+postRouter.post("/", upload.single("image"), postController.createPostController);
+
+
+// GET /api/posts/ [protected]
+
+postRouter.get("/", postController.getPostController);
+
+
+// GET /api/posts/details/:postid
+// return an detail about specific post with the id. also check whether the post belongs to the user that the request come from.
+
+postRouter.get("/details/:postId", postController.getPostDetails);
+
 
 module.exports = postRouter
